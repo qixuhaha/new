dcbbf839440081c23c6bb7bfbc40c6d08f5ce97e
qixuhaha <18645242510@163.com>
2018-10-23 19:29:26 +0800

    init1

---
+++ config/jest/babelTransform.js
@@ -0,0 +1,8 @@
+module.exports = babelJest.createTransformer({
+    presets: [
+    //   require.resolve('../presets.js'),
+      require.resolve('babel-plugin-transform-decorators-legacy'),
+    ],
+    babelrc: false,
+  });
+  
